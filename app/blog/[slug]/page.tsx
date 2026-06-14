446727134000a3856d0f4bb7a2e5be1d8867990d
Navraj Awasthi <awasthintr07@gmail.com>
2026-06-15 00:45:50 +0545

    added slug

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,10 +1,8 @@
-const page = () => {
-  return (
-    <div>
-      <h1>My Blog Post</h1>
-      <p>what`s up guys</p>
-    </div>
-  );
-};
+export default function Page() {
+  return <h1>Hello, Blog Post Page </h1>;
+}
+// Use the searchParams prop when you need search parameters to load data for the page (e.g. pagination, filtering from a database).
 
-export default page;
+// Use useSearchParams when search parameters are used only on the client (e.g. filtering a list already loaded via props).
+
+// As a small optimization, you can use new URLSearchParams(window.location.search) in callbacks or event handlers to read search params without triggering re-renders.
