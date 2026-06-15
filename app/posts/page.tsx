a83e10ee59ec335076674adba876613aa61526b1
Navraj Awasthi <awasthintr07@gmail.com>
2026-06-15 19:39:28 +0545

    server components vs client components

---
+++ app/posts/page.tsx
@@ -0,0 +1,23 @@
+async function getPOsts() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  return res.json();
+}
+
+export default async function Page() {
+  const posts = await getPOsts();
+  return (
+    <div>
+      <h1>Posts</h1>
+      {posts.slice(0, 5).map((post) => (
+        <p key={post.id}> {post.title} </p>
+      ))}
+    </div>
+  );
+}
+
+
+// Why is this a Server Component?
+// No "use client" directive.
+// Runs on the server.
+// Can fetch data directly from APIs or databases.
+// HTML is generated on the server and sent to the browser.
