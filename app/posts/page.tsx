22481c2c4c5c8094c580ac096f3d8cb911417b7c
Navraj Awasthi <awasthintr07@gmail.com>
2026-06-17 16:52:33 +0545

    fetching data in server component

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,23 +1,37 @@
-async function getPOsts() {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-  return res.json();
-}
+// async function getPOsts() {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+//   return res.json();
+// }
 
-export default async function Page() {
-  const posts = await getPOsts();
-  return (
-    <div>
-      <h1>Posts</h1>
-      {posts.slice(0, 5).map((post) => (
-        <p key={post.id}> {post.title} </p>
-      ))}
-    </div>
-  );
-}
-
+// export default async function Page() {
+//   const posts = await getPOsts();
+//   return (
+//     <div>
+//       <h1>Posts</h1>
+//       {posts.slice(0, 5).map((post) => (
+//         <p key={post.id}> {post.title} </p>
+//       ))}
+//     </div>
+//   );
+// }
 
 // Why is this a Server Component?
 // No "use client" directive.
 // Runs on the server.
 // Can fetch data directly from APIs or databases.
 // HTML is generated on the server and sent to the browser.
+
+export default function Page() {
+  const data = await fetch(`https://api.vercel.app/blog`);
+  const posts = await data.json();
+  return (
+    <ul>
+      {posts.map((post: any) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+
+// fetching data 
